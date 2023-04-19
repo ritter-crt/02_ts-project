65054d2e2c89151afa29b382a3cb871bca0d2794
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-04-19 15:22:00 +0200

    sorting array of nums

--- build/index.js
+++ build/index.js
@@ -1,2 +1,26 @@
-"use strict";
-console.log('hi there');
+'use strict';
+class Sorter {
+  constructor(collection) {
+    this.collection = collection;
+  }
+  sort() {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // comparing numbers each go around
+        // compares leftHandSide [j] to rightHandSide [j+1]
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          // takes the rightHandSide and throws it over to the left
+          this.collection[j] = this.collection[j + 1];
+          // takes the leftHandSide and throws it over to the right
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([6, 98, 32, -3]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('hi there');
+class Sorter {
+  constructor(public collection: number[]) {}
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // comparing numbers each go around
+        // compares leftHandSide [j] to rightHandSide [j+1]
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          // takes the rightHandSide and throws it over to the left
+          this.collection[j] = this.collection[j + 1];
+          // takes the leftHandSide and throws it over to the right
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+// equal to :
+// class Sorter {
+//   collection: number[];
+
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+
+const sorter = new Sorter([6, 98, 32, -3]);
+sorter.sort();
